ca01b50b6dc2c1a61a37df189f2b5bda360e1cc8
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-09-26 17:14:54 +0200

    Final version of the l200_result plot

--- src/plot.jl
+++ src/plot.jl
@@ -1,8 +1,13 @@
-using Plots
+using StatsBase: weights, Histogram
+import PythonPlot as plt
 
-# FIXME: this assumes main.jl has run, needs to be rewritten
+# FIXME: this assumes main.jl has run! it's supposed to be run interactively
+# after the fit
 
-function n_counts_perkev_obs(partitions::Table, energy, Γ12, B, α)::Float64
+# this function calculates the observable counts density at a certain energy
+# and given the samples. "density" means that there is no integral over energy
+# (i.e. no multiplication by ΔE)
+function counts_density_obs(partitions, energy, Γ12, B, α; bkg=true, signal=true)
     Qββ = 2039.04 # keV
     N_A = 6.02214
     m_76 = 75.9214 # g/mol
@@ -20,81 +25,137 @@ function n_counts_perkev_obs(partitions::Table, energy, Γ12, B, α)::Float64
     _Δk = rand.(Normal.(Δk(:val), Δk(:err)))
     _σk = rand.(Normal.(σk(:val), σk(:err)))
 
-    μk = μbk .+ μsk .* pdf.(Normal.(Qββ .+ _Δk, _σk), energy)
+    μk = (bkg ? μbk : 0) .+ (signal ? μsk .* pdf.(Normal.(Qββ .+ _Δk, _σk), energy) : 0)
 
     return sum(μk)
 end
 
-function n_counts_l200_post(energy, samples)
+# this function calculates the posterior of the counts density/(kg yr) at a certain energy
+function counts_density_kgyr_l200_post(energy, samples; kwargs...)
+    @info "calculating posterior for energy $energy"
     partitions = data[:legend200].partitions
-    f = s -> n_counts_perkev_obs(partitions, energy, s.Γ12, s.legend200_B, s.α)
+    f = s -> counts_density_obs(partitions, energy, s.Γ12, s.legend200_B, s.α; kwargs...)
     return broadcast(f, samples.v) ./ sum(partitions.exposure)
 end
 
-function n_counts_l200_mode(energy, mode)
+# this function calculates the counts density/(kg yr) at the marginal mode
+function counts_density_kgyr_l200_mode(energy, mode; kwargs...)
     partitions = data[:legend200].partitions
     m = mode
-    n_cts = n_counts_perkev_obs(partitions, energy, m.Γ12, m.legend200_B, m.α)
+    n_cts = counts_density_obs(partitions, energy, m.Γ12, m.legend200_B, m.α; kwargs...)
     return n_cts ./ sum(partitions.exposure)
 end
 
 function plot_l200_result()
+    # we don't really need all samples...
+    _samples = samples[1:20_000]
+    _weights = weights(_samples.weight)
 
-    _samples = samples[end-20_000:end]
-    _weights = Weights(_samples.weight)
+    # for the background, we just need one (random) energy point
+    # we'll compute the 68% smallest CI
+    posterior = counts_density_kgyr_l200_post(2000, _samples, signal=false)
+    _int = BAT.smallest_credible_intervals(posterior, _weights, nsigma_equivalent=1)
+    length(_int) != 1 && @warn "[$E keV] 68% interval is disjoint"
+    b_68 = first(_int)
 
-    energies = [1930, collect(2034:0.3:2043)..., 2190]
-    i68 = Vector(undef, length(energies))
-    i95 = Vector(undef, length(energies))
+    # let's use a narrow region around Qbb for the 90% CI on the signal
+    energies = 2034:0.3:2043
 
+    s_90 = Vector(undef, length(energies))
+
+    # let's speed this up with multi-threading
     Threads.@threads for i in 1:length(energies)
         E = energies[i]
-        @info "calculating posterior for energy $E"
-        posterior = n_counts_l200_post(E, _samples)
+        posterior = counts_density_kgyr_l200_post(E, _samples, bkg=false)
 
-        _int = BAT.smallest_credible_intervals(posterior, _weights, nsigma_equivalent=1)
-        length(_int) != 1 && @warn "[$E keV] 68% interval is disjoint"
-        i68[i] = first(_int)
-
-        _int = BAT.smallest_credible_intervals(posterior, _weights, nsigma_equivalent=2)
-        length(_int) != 1 && @warn "[$E keV] 95% interval is disjoint"
-        i95[i] = first(_int)
+        # no need for smalles interval here
+        # _int = BAT.smallest_credible_intervals(posterior, _weights, nsigma_equivalent=1.64)
+        s_90[i] = 0..quantile(posterior, _weights, 0.9)
     end
 
-    plot(
-        xlim=[1930, 2190],
-        # ylim=(1E-4, 1E-1), yscale=:log10,
-        ylim=(0, 0.01),
-        xlabel="Energy [keV]", ylabel="BI [cts / (keV kg yr)]"
+    # plot!
+
+    fig, ax = plt.subplots(figsize=(5, 2.5))
+
+    band = plt.fill_between(
+        [1930, 2190], fill(b_68.left, 2), fill(b_68.right, 2),
+        linewidth=0, alpha=0.5, color="#228833",
+        label="68% C.I.",
+        zorder=0,
     )
 
-    plot!(
+    b_mode = counts_density_kgyr_l200_mode(2000, mode(_samples), signal=false)
+    line, = plt.plot(
+        [1930, 2190], [b_mode, b_mode],
+        color="#228833",
+        zorder=1,
+    )
+
+    ax.set_xlim(1930, 2190)
+    ax.set_xlabel("Energy [keV]", loc="right")
+    ax.set_ylabel("Counts / (keV kg yr)")
+    # ylim=(1E-4, 1E-1), yscale=:log10,
+
+    limit_area = plt.fill_between(
         energies,
-        getproperty.(i95, :left), fillrange=getproperty.(i95, :right),
-        color="orange", fillalpha=0.3, linewidth=0,
+        getproperty.(s_90, :left), getproperty.(s_90, :right),
+        linewidth=0, color="#4477AA",
+        label="90% C.I.",
     )
 
-    plot!(
-        energies,
-        getproperty.(i68, :left), fillrange=getproperty.(i68, :right),
-        color="green", fillalpha=0.3, linewidth=0,
+    # plot the data
+    unbinned = false
+
+    # re-add that count in the gamma line
+    x = [data[:legend200].events.energy..., 2118.465]
+    data_art = nothing
+
+    if unbinned
+        data_art, stemlines, baseline = ax.stem(
+            x, fill(0, length(x)), "#CC3311",
+            basefmt="none",
+        )
+        data_art.set_markersize(1.5)
+        # NOTE: comment the following if the stem has a length!
+        data_art.set_zorder(99)
+        data_art.set_clip_on(false)
+
+        stemlines.set_linewidth(1)
+        ax.set_ylim(0, 0.013)
+    else
+        w = 1/sum(data[:legend200].partitions.exposure)
+        _, _, data_art = plt.hist(
+            x,
+            weights=fill(w, length(x)),
+            bins=1930:1:2190,
+            color="#CC3311",
+        )
+        ax.set_ylim(1E-4, 2)
+        ax.set_yscale("log")
+    end
+
+    # now this mess to plot the excluded regions
+    plt.hist(
+        [2104, 2119],
+        bins=[2099, 2109, 2114, 2124],
+        weights=[9, 9],
+        color="black", alpha=0.4,
     )
 
-    plot!(
-        energies,
-        broadcast(e -> n_counts_l200_mode(e, mode(samples)), energies),
-        color="black",
-        # st=:scatter,
+    ax.legend(
+        [data_art, (band, line), limit_area],
+        [
+            raw"LEGEND-200 data [48.3 kg yr]",
+            raw"$6.2^{+1.8}_{-2.5} \times 10^{-4}$ cts / (keV kg yr) [68% C.I.]",
+            raw"$T^{0\nu}_{1/2} > 1.8 \times 10^{26}$ yr [90% C.I.]",
+        ],
+        loc="upper left",
+        fontsize="small",
     )
 
-    x = data[:legend200].events.energy
+    plt.tight_layout()
+    fig.savefig("l200-result.pdf", bbox_inches="tight")
 
-    plot!(
-        x, fill(2E-3, length(x)), label="Data",
-        st=:sticks, linewidth=2,
-        markershape=:dtriangle, markerstrokewidth=0,
-        color="black",
-    )
 end
 
 # function plot_marginals()
